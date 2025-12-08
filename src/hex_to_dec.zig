27347814c4740fb5bcf2f5caca105f2640d529c2
Harald Magnusson <harald.magnusson24@gmail.com>
2025-12-08 21:20:53 +0100

    add hex_to_dec.zig

---
+++ src/hex_to_dec.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const printing = @import("printing.zig");
+
+pub fn main() !void {
+    var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_allocator.deinit();
+    const arena = arena_allocator.allocator();
+
+    var args = try std.process.argsWithAllocator(arena);
+    defer args.deinit();
+
+    _ = args.skip(); // skip executable name
+    var arg_count: u32 = 0;
+    while (args.next()) |arg| {
+        arg_count += 1;
+        std.log.debug("arg {d}: {s}", .{ arg_count, arg });
+
+        const base_16 = 16;
+        const nombre = std.fmt.parseInt(u256, arg, base_16) catch |err| {
+            std.log.debug("invalid input: {s}", .{arg});
+            return err;
+        };
+
+        try printing.bufferedPrint("{d}\n", .{nombre});
+    }
+}
