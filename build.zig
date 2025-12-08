a6f5a67d145a32b884e726d85e80e0deccf95dc8
Harald Magnusson <harald.magnusson24@gmail.com>
2025-12-08 21:46:18 +0100

    extend build.zig to build multiple executables

--- build.zig
+++ build.zig
@@ -4,29 +4,52 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.createModule(.{
-        .root_source_file = b.path("src/dec_to_hex.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const ExecutableSource = struct {
+        path: []const u8,
+        name: []const u8,
+        description: []const u8,
+    };
+    const executables = comptime [_]ExecutableSource{
+        .{
+            .path = "src/dec_to_hex.zig",
+            .name = "dectohex",
+            .description = "convert from decimal to hexadecimal",
+        },
+        .{
+            .path = "src/hex_to_dec.zig",
+            .name = "hextodec",
+            .description = "convert from hexadecimal to decimal",
+        },
+    };
 
-    const exe = b.addExecutable(.{
-        .name = "dectohex",
-        .root_module = mod,
-    });
+    inline for (executables) |executable| {
+        const mod = b.createModule(.{
+            .root_source_file = b.path(executable.path),
+            .target = target,
+            .optimize = optimize,
+        });
 
-    b.installArtifact(exe);
-    const run_step = b.step("dectohex", "convert from hexadecimal to decimal");
-    const exe_artifact = b.addRunArtifact(exe);
-    if (b.args) |args| {
-        exe_artifact.addArgs(args);
+        const exe = b.addExecutable(.{
+            .name = executable.name,
+            .root_module = mod,
+        });
+
+        b.installArtifact(exe);
+        const exe_artifact = b.addRunArtifact(exe);
+
+        if (b.args) |args| {
+            exe_artifact.addArgs(args);
+        }
+
+        const run_step = b.step(executable.name, executable.description);
+        run_step.dependOn(&exe_artifact.step);
+
+        const test_mod = b.addTest(.{
+            .root_module = mod,
+        });
+        const test_name = "test " ++ executable.name;
+        const test_step = b.step(test_name, test_name);
+        const test_artifact = b.addRunArtifact(test_mod);
+        test_step.dependOn(&test_artifact.step);
     }
-    run_step.dependOn(&exe_artifact.step);
-
-    const test_mod = b.addTest(.{
-        .root_module = mod,
-    });
-    const test_step = b.step("test", "test dectohex");
-    const test_artifact = b.addRunArtifact(test_mod);
-    test_step.dependOn(&test_artifact.step);
 }
