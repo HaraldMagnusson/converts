bd74fd8f338a4dc1a89a5ced0ea3025ae4db4f8f
Harald Magnusson <harald.magnusson24@gmail.com>
2025-12-08 21:13:33 +0100

    implement dec_to_hex

--- src/dec_to_hex.zig
+++ src/dec_to_hex.zig
@@ -2,5 +2,25 @@ const std = @import("std");
 const printing = @import("printing.zig");
 
 pub fn main() !void {
-    try printing.bufferedPrint("foo\n", .{});
+    var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_allocator.deinit();
+    const arena = arena_allocator.allocator();
+
+    var args = try std.process.argsWithAllocator(arena);
+    defer args.deinit();
+
+    _ = args.skip(); // skip executable name
+    var arg_count: u32 = 0;
+    while (args.next()) |arg| {
+        arg_count += 1;
+        std.log.debug("arg {d}: {s}", .{ arg_count, arg });
+
+        const base_ten = 10;
+        const nombre = std.fmt.parseInt(u256, arg, base_ten) catch |err| {
+            std.log.debug("invalid input: {s}", .{arg});
+            return err;
+        };
+
+        try printing.bufferedPrint("{X}\n", .{nombre});
+    }
 }
